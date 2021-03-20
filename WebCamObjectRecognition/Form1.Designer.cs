86b4676b19059049ac760af865a0379cb035ed9f
Kirill Sakov <sakoff.kirill@yandex.ru>
2021-03-20 22:39:33 +0300

    first uploading

---
+++ WebCamObjectRecognition/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WebCamObjectRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.camerasStripComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.repeatButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.modeButton = new System.Windows.Forms.ToolStripButton();
+            this.alarmButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.startButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.recognitionButton = new System.Windows.Forms.ToolStripButton();
+            this.resumeButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.openButton = new System.Windows.Forms.ToolStripButton();
+            this.pictureCameraBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.camerasStripComboBox,
+            this.repeatButton,
+            this.toolStripSeparator1,
+            this.toolStripButton1,
+            this.startButton,
+            this.toolStripSeparator2,
+            this.modeButton,
+            this.alarmButton,
+            this.toolStripSeparator3,
+            this.recognitionButton,
+            this.resumeButton,
+            this.toolStripSeparator4,
+            this.openButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1280, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(117, 22);
+            this.toolStripLabel1.Text = "Доступные камеры:";
+            // 
+            // camerasStripComboBox
+            // 
+            this.camerasStripComboBox.Name = "camerasStripComboBox";
+            this.camerasStripComboBox.Size = new System.Drawing.Size(160, 25);
+            // 
+            // repeatButton
+            // 
+            this.repeatButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.repeatButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.repeatButton.Name = "repeatButton";
+            this.repeatButton.Size = new System.Drawing.Size(23, 22);
+            this.repeatButton.Text = "↻";
+            this.repeatButton.Click += new System.EventHandler(this.repeatButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // modeButton
+            // 
+            this.modeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.modeButton.Image = ((System.Drawing.Image)(resources.GetObject("modeButton.Image")));
+            this.modeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.modeButton.Name = "modeButton";
+            this.modeButton.Size = new System.Drawing.Size(140, 22);
+            this.modeButton.Text = "Отслеживать движение";
+            this.modeButton.Click += new System.EventHandler(this.modeButton_Click);
+            // 
+            // alarmButton
+            // 
+            this.alarmButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.alarmButton.Image = ((System.Drawing.Image)(resources.GetObject("alarmButton.Image")));
+            this.alarmButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.alarmButton.Name = "alarmButton";
+            this.alarmButton.Size = new System.Drawing.Size(250, 22);
+            this.alarmButton.Text = "Уведомлять о движении с камеры на почту";
+            this.alarmButton.ToolTipText = "Включить уведомления";
+            this.alarmButton.Click += new System.EventHandler(this.alarmButton_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // startButton
+            // 
+            this.startButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.startButton.Image = ((System.Drawing.Image)(resources.GetObject("startButton.Image")));
+            this.startButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(35, 22);
+            this.startButton.Text = "Start";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(35, 22);
+            this.toolStripButton1.Text = "Stop";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // recognitionButton
+            // 
+            this.recognitionButton.BackColor = System.Drawing.SystemColors.Info;
+            this.recognitionButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.recognitionButton.Image = ((System.Drawing.Image)(resources.GetObject("recognitionButton.Image")));
+            this.recognitionButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.recognitionButton.Name = "recognitionButton";
+            this.recognitionButton.Size = new System.Drawing.Size(123, 22);
+            this.recognitionButton.Text = "Распознать объекты";
+            this.recognitionButton.Click += new System.EventHandler(this.recognitionButton_Click);
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.resumeButton.Image = ((System.Drawing.Image)(resources.GetObject("resumeButton.Image")));
+            this.resumeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(53, 22);
+            this.resumeButton.Text = "Рестарт";
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // openButton
+            // 
+            this.openButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+            this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(188, 22);
+            this.openButton.Text = "Открыть изображение из файла";
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pictureCameraBox
+            // 
+            this.pictureCameraBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureCameraBox.Location = new System.Drawing.Point(0, 25);
+            this.pictureCameraBox.Name = "pictureCameraBox";
+            this.pictureCameraBox.Size = new System.Drawing.Size(1280, 720);
+            this.pictureCameraBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureCameraBox.TabIndex = 2;
+            this.pictureCameraBox.TabStop = false;
+            this.pictureCameraBox.DoubleClick += new System.EventHandler(this.pictureCameraBox_DoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|*.jpg|PNG|*.png|Bitmap|*.bmp";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 745);
+            this.Controls.Add(this.pictureCameraBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Object Recognition";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox camerasStripComboBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton startButton;
+        private System.Windows.Forms.PictureBox pictureCameraBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton recognitionButton;
+        private System.Windows.Forms.ToolStripButton resumeButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton modeButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton openButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripButton repeatButton;
+        private System.Windows.Forms.ToolStripButton alarmButton;
+    }
+}
+
---
+++ WebCamObjectRecognition/Form1.cs
@@ -0,0 +1,373 @@
+﻿using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WebCamObjectRecognition
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        // create motion detector
+        MotionDetector detector = new MotionDetector(
+            new TwoFramesDifferenceDetector(),
+            new MotionBorderHighlighting());
+
+        static bool isMotionDetectorOn = false;
+        static bool isAlarmNotificationsOn = false;
+        static bool isMessageSend = false;
+        private string fileName = String.Empty;
+
+        YoloWrapper yolo = new YoloWrapper(
+            "yolo-weights\\yolov3.cfg", 
+            "yolo-weights\\yolov3.weights", 
+            "yolo-weights\\coco.names"
+            );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count != 0)
+            {
+                foreach (FilterInfo cam in videoDevices)
+                {
+                    camerasStripComboBox.Items.Add(cam.Name);
+                }
+                camerasStripComboBox.SelectedIndex = 0;                
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (camerasStripComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Необходимо выбрать камеру!", "Ошибка!", MessageBoxButtons.OK);
+                return;
+            }
+
+            try
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[camerasStripComboBox.SelectedIndex].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+                CameraOn(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при попытке подключения к камере!\nОписание ошибки: " + ex.Message, "Ошибка!", MessageBoxButtons.OK);
+                return;
+
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+
+            if (isMessageSend)
+            {
+                videoSource.SignalToStop();
+                return;
+            }
+
+            if (isMotionDetectorOn)
+            {
+                float motion_lvl;
+                motion_lvl = detector.ProcessFrame(eventArgs.Frame);
+
+                // Если включено "Уведомлять о движении на камере на почту"
+                if (isAlarmNotificationsOn)
+                {
+                    if (motion_lvl > 0.1f)
+                    {
+                        // Проверяем, существует ли папка catchPhotos
+                        if (!Directory.Exists(@".\\catchPhotos"))
+                        {
+                            // Если нет, создаём
+                            Directory.CreateDirectory(@".\\catchPhotos");
+                        }
+
+                        // Отлавливаем и сохраняем кадр на диск
+                        pictureCameraBox.Image.Save($".\\catchPhotos\\сatchPhoto{motion_lvl}.bmp", ImageFormat.Bmp);
+
+                        // Составляем и отправляем письмо
+
+                        #region Составление и отправка письма
+                        var message = new MimeMessage();
+                        message.From.Add(new MailboxAddress("Camera App", "kirill.rubinov@yandex.ru"));
+                        message.To.Add(new MailboxAddress("Admin", "sakoff.kirill@yandex.ru"));
+                        message.Subject = "Alarm";
+
+                        message.Body = new TextPart("plain")
+                        {
+                            Text = @"Hey men,
+
+                            On your camera detected some motion
+
+                            -- Your app"
+                        };
+
+                        // create an image attachment for the file located at path
+                        var attachment = new MimePart("image", "gif")
+                        {
+                            Content = new MimeContent(File.OpenRead($".\\catchPhotos\\сatchPhoto{motion_lvl}.bmp")),
+                            ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                            ContentTransferEncoding = ContentEncoding.Base64,
+                            FileName = Path.GetFileName($".\\catchPhotos\\сatchPhoto{motion_lvl}.bmp")
+                        };
+
+                        // now create the multipart/mixed container to hold the message text and the
+                        // image attachment
+                        var multipart = new Multipart("mixed");
+                        multipart.Add(message.Body);
+                        multipart.Add(attachment);
+
+                        // now set the multipart/mixed as the message body
+                        message.Body = multipart;
+
+                        using (var client = new SmtpClient())
+                        {
+                            client.Connect("smtp.yandex.ru", 465, true);
+
+                            // Note: only needed if the SMTP server requires authentication
+                            client.Authenticate("example@yandex.ru", "examplepassword");
+
+                            client.Send(message);
+                            client.Disconnect(true);
+                        }
+
+                        isMessageSend = true;
+                        #endregion
+
+                    }
+                }                
+            }
+
+            Bitmap tmpBMP = (Bitmap)eventArgs.Frame.Clone();
+            tmpBMP.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            try
+            {
+                pictureCameraBox.Image = tmpBMP;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (videoSource != null)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                Environment.Exit(0);
+            }
+        }
+
+        private void recognitionButton_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null || pictureCameraBox.Image != null)
+            {
+                if (videoSource != null)
+                    videoSource.Stop();
+
+                MemoryStream memoryStream = new MemoryStream();
+                pictureCameraBox.Image.Save(memoryStream, ImageFormat.Bmp);
+                List<YoloItem> items = yolo.Detect(memoryStream.ToArray()).ToList<YoloItem>();
+
+                Bitmap finalImage = new Bitmap(pictureCameraBox.Image);
+
+                Graphics graph = Graphics.FromImage(finalImage);
+                Font font = new Font("Consolas", 20, FontStyle.Bold);
+                SolidBrush brush = new SolidBrush(Color.Yellow);
+
+                foreach (YoloItem item in items)
+                {
+                    Point rectPoint = new Point(item.X, item.Y);
+                    Size rectSize = new Size(item.Width, item.Height);
+
+                    Rectangle rect = new Rectangle(rectPoint, rectSize);
+
+                    Pen pen = new Pen(Color.Yellow, 2);
+
+                    graph.DrawRectangle(pen, rect);
+                    graph.DrawString(item.Type, font, brush, rectPoint);
+                }
+
+                pictureCameraBox.Image = finalImage;
+            }
+        }
+
+        private void resumeButton_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null)
+                videoSource.Start();
+        }
+
+        
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null)
+            {
+                videoSource.Stop();
+                isMessageSend = false;
+                CameraOn(false);
+            }
+        }
+
+        private void CameraOn(bool is_on)
+        {
+            if (!is_on)
+                videoSource = null;
+
+            startButton.Enabled = !is_on;
+            camerasStripComboBox.Enabled = !is_on;
+            repeatButton.Enabled = !is_on;
+        }
+        
+        private void modeButton_Click(object sender, EventArgs e)
+        {
+            isMotionDetectorOn = !isMotionDetectorOn;
+            if (isMotionDetectorOn)
+            {
+                modeButton.BackColor = SystemColors.ActiveBorder;
+            }
+            else if (!isMotionDetectorOn)
+            {
+                modeButton.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void pictureCameraBox_DoubleClick(object sender, EventArgs e)
+        {
+            pictureCameraBox.Image = null;
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog1.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                pictureCameraBox.Image = Image.FromFile(fileName);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            try
+            {
+                pictureCameraBox.Image = Image.FromFile(files[0]);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка при импортировании изображения.", "Ошибка!", MessageBoxButtons.OK);
+                return;
+            }            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case (Keys.V | Keys.Control):
+                    {
+                        if (Clipboard.ContainsImage())
+                        {
+                            pictureCameraBox.Image = Clipboard.GetImage();
+                        }
+                        else
+                        {
+
+                            string[] data = 
+                                (string[])Clipboard.GetData(DataFormats.FileDrop);
+
+                            try
+                            {
+                                pictureCameraBox.Image = Image.FromFile(data[0]);
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Ошибка при импортировании изображения.", "Ошибка!", MessageBoxButtons.OK);
+                                return;
+                            }
+                        }                        
+                        break;
+                    }
+
+                case (Keys.C | Keys.Control):
+                    {
+                        try
+                        {
+                            Clipboard.SetImage(pictureCameraBox.Image);
+                            //MessageBox.Show("Картинка сохранена в буфер обмена.", "Успешно!", MessageBoxButtons.OK);
+                        }
+                        catch (Exception)
+                        {
+                            
+                        }
+                        break;
+                    }
+            }
+        }
+
+        private void repeatButton_Click(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count != 0)
+            {
+                foreach (FilterInfo cam in videoDevices)
+                {
+                    camerasStripComboBox.Items.Add(cam.Name);
+                }
+                camerasStripComboBox.SelectedIndex = 0;
+            }
+            if (videoDevices.Count == 0)
+            {
+                camerasStripComboBox.Items.Clear();
+                camerasStripComboBox.Text = "";
+            }
+        }
+
+        private void alarmButton_Click(object sender, EventArgs e)
+        {
+            isAlarmNotificationsOn = !isAlarmNotificationsOn;
+            if (isAlarmNotificationsOn)
+            {
+                alarmButton.BackColor = SystemColors.ActiveBorder;
+            }
+            else if (!isAlarmNotificationsOn)
+            {
+                alarmButton.BackColor = DefaultBackColor;
+            }
+        }
+    }
+}
